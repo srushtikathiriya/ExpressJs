21f88f75eaae539062a65998276e46e5d452d64f
srushti <srushtikathiriya2003@gmail.com>
2024-08-06 21:39:36 +0530

    06-08-2024

--- server.js
+++ server.js
@@ -6,17 +6,49 @@ server.get('/', (req, res) => {
         res.end();
 })
 
-server.get('/login', (req, res) => {
-    res.write("Welcome Express Js");
-    res.end();
+// server.get('/', (req, res) => {
+//     res.send("Get Method -1")
+// })
+
+server.get("/user",(req,res)=>{
+    res.status(200);
+    res.json({msg:"User get method"});
 })
 
-server.get('/login', (req, res) => {
-    res.json({msg:"Login page post"});
-    res.end();
+server.post("/user",(req,res)=>{
+    res.status(201);
+    res.json({msg:"User post method"});
+})
+
+server.put("/user",(req,res)=>{
+    // res.status(201);
+    res.json({msg:"User put method"});
+})
+
+server.patch('/user', (req, res) => {
+    res.json({msg:"User patch method"});
+})
+
+server.delete('/user', (req, res) => {
+    res.json({msg:"User Delete method"});
+})
+
+server.patch("/admin",(req,res)=>{
+    res.json({msg:"Admin patch method"});
+})
+
+server.delete("/admin",(req,res)=>{
+    res.json({msg:"Admin Delete method"});
 })
 
 server.listen(3500,()=>{
-    console.log(`Server Start at http://localhost:3500`);
-    
+    console.log(`Server Start at http://localhost:3500`);   
 })
+
+
+/*
+    git checkout -b sk-1
+    git add .
+    git commit -m "Your commit"
+    git push -u origin sk-1
+*/
