1002f2b1615d16d7dfa5c72bdd3d5b08f4e62634
srushti <srushtikathiriya2003@gmail.com>
2024-08-08 09:38:41 +0530

    07-08-2024

--- server.js
+++ server.js
@@ -25,40 +25,6 @@ server.get('/',middleware, (req, res) => {
         res.end();
 })
 
-// server.get('/', (req, res) => {
-//     res.send("Get Method -1")
-// })
-
-server.get("/user",(req,res)=>{
-    res.status(200);
-    res.json({msg:"User get method"});
-})
-
-server.post("/user",(req,res)=>{
-    res.status(201);
-    res.json({msg:"User post method"});
-})
-
-server.put("/user",(req,res)=>{
-    // res.status(201);
-    res.json({msg:"User put method"});
-})
-
-server.patch('/user', (req, res) => {
-    res.json({msg:"User patch method"});
-})
-
-server.delete('/user', (req, res) => {
-    res.json({msg:"User Delete method"});
-})
-
-server.patch("/admin",(req,res)=>{
-    res.json({msg:"Admin patch method"});
-})
-
-server.delete("/admin",(req,res)=>{
-    res.json({msg:"Admin Delete method"});
-})
 
 server.get("/friend",(req,res)=>{
     res.status(200);
