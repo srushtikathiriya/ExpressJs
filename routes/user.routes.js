f1be3184a5b50a491774a6d13619ecd1047366be
srushti <srushtikathiriya2003@gmail.com>
2024-08-23 04:57:17 +0530

    23-08-2024

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,7 +4,9 @@ const {
     registerUser,
     loginUser,
     userProfile,
-    updateProfile
+    updateProfile,
+    changePassword,
+    deleteUser
 } = require("../controller/user.controller");
 const {verifyToken} = require("../helpers/tokenVerify")
 
@@ -12,5 +14,7 @@ userRoutes.post("/register",registerUser);
 userRoutes.post("/login",loginUser);
 userRoutes.get("/me",verifyToken,userProfile);
 userRoutes.put("/update-profile",verifyToken,updateProfile);
+userRoutes.put("/change-password", verifyToken, changePassword);
+userRoutes.delete('/delete-user',verifyToken, deleteUser);
 
 module.exports = userRoutes;
